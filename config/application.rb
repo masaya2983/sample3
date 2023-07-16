54dc0f462fd8072bd6936f0b9676701c1047ab77
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-16 14:26:58 +0000

    1日で基本が身につく

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module Sample2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -18,5 +18,6 @@ module Sample2
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = 'Tokyo' 
   end
 end
