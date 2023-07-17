12e54eca6f9ace5affaf993d184581ec69074cbd
Masaya Ohtsuka <127204441+masaya2983@users.noreply.github.com>
2023-07-17 06:10:04 +0000

    1日で基本が身につく

--- app/models/diary.rb
+++ app/models/diary.rb
@@ -1,4 +1,16 @@
 class Diary < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
+  validates :proper_title_and_body
+
+  private
+
+    def  proper_title_and_body
+      unless title.starts_with?('今日')
+        errors.add(:title,'は「今日」から始めてください。')
+      end
+      unless body.ends_with?('、')
+        errors.add(:body,'は句点(。)で終了してください。')
+      end
+    end
 end
